148212d5e6a49362347fb68edba23208eab8316b
guoqiang0236 <1518120457@qq.com>
2025-08-18 19:51:35 +0800

    多线程服务器例子

---
+++ c++多线程/thread_msg_server/xmsg_server.cpp
@@ -0,0 +1,31 @@
+
+#include "xmsg_server.h"
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace this_thread;
+//处理消息的线程入口函数
+void XMsgServer::Main()
+{
+    while (!is_exit())
+    {
+        sleep_for(10ms);
+        unique_lock<mutex> lock(mux_);
+        if (msgs_.empty())
+            continue;
+        while (!msgs_.empty())
+        {
+            //消息处理业务逻辑
+            cout << "recv : " << msgs_.front() << endl;
+            msgs_.pop_front();
+        }
+
+    }
+}
+
+//给当前线程发消息
+void XMsgServer::SendMsg(std::string msg)
+{
+    unique_lock<mutex> lock(mux_);
+    msgs_.push_back(msg);
+}
